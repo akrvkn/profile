2f7f35440df0d96d7722679ba01e890cf1e38320
akrvkn <akrvkn@yandex.ru>
2020-06-23 06:05:49 +0300

    embed script

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -1,3 +1,4 @@
+console.log('top');
 let tag = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="https://www.mosturflot.ru/articles/31223/">Электронная запись на посещение Офисов</a>';
 let hDiv = document.getElementById('top-news');
 hDiv.innerHTML = tag;
