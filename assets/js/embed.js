4013cd966c1a05bfd9e1e37efc9a5ebe89c908cf
akrvkn <akrvkn@yandex.ru>
2020-07-02 17:56:49 +0300

    new script

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -1,4 +1,4 @@
-et tag = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="https://www.mosturflot.ru/articles/31385//">Информация о месте посадки на теплоход.</a>';
+let tag = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="https://www.mosturflot.ru/articles/31385//">Информация о месте посадки на теплоход.</a>';
 let hDiv = document.getElementById('top-news');
 if(hDiv) {
     hDiv.innerHTML = tag;
