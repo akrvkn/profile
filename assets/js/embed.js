8b55c031e5976887d2fce1ca9a0662c324163de2
akrvkn <akrvkn@yandex.ru>
2020-09-09 13:53:10 +0300

    tablo srv

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -1,6 +1,6 @@
-let tag1 = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="/articles/31385/">Информация о месте посадки на теплоход.</a>';
-let tag1 = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="/tablo-severnogo-rechnogo-vokzala/">Табло Северного Речного Вокзала</a>';
+let tag1 = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="/articles/31385/">Информация о месте посадки на теплоход.</a><br>';
+let tag2 = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="/tablo-severnogo-rechnogo-vokzala/">Табло Северного Речного Вокзала</a>';
 let hDiv = document.getElementById('top-news');
 if(hDiv) {
-    hDiv.innerHTML = tag1;
+    hDiv.innerHTML = tag1 + tag2;
 }
