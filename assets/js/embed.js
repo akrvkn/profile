ad6ac42d96c868fb937a5d41264a918f60c8df3a
akrvkn <akrvkn@yandex.ru>
2020-09-09 13:58:56 +0300

    tablo srv info

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -1,4 +1,4 @@
-let tag1 = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="/articles/31385/">Информация о месте посадки на теплоход.</a><br>';
+let tag1 = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="/articles/vnimanie-o-meste-posadki-na-teplokhody/">Информация о месте посадки на теплоход.</a><br>';
 let tag2 = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="/tablo-severnogo-rechnogo-vokzala/">Табло Северного Речного Вокзала</a>';
 let hDiv = document.getElementById('top-news');
 if(hDiv) {
