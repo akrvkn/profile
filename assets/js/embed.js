ad8d3c9f3a4b8d1da961863ef0b958f8b59912f2
akrvkn <akrvkn@yandex.ru>
2020-09-09 13:51:42 +0300

    tablo srv

--- assets/js/embed.js
+++ assets/js/embed.js
@@ -1,4 +1,5 @@
-let tag1 = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="https://www.mosturflot.ru/articles/31385//">Информация о месте посадки на теплоход.</a>';
+let tag1 = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="/articles/31385/">Информация о месте посадки на теплоход.</a>';
+let tag1 = '<a style="font-size: 24px;font-weight: bold;color: #0962a8;text-decoration: none;" href="/tablo-severnogo-rechnogo-vokzala/">Табло Северного Речного Вокзала</a>';
 let hDiv = document.getElementById('top-news');
 if(hDiv) {
     hDiv.innerHTML = tag1;
